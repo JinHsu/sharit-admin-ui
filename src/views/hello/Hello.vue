20a8e7fcd2e741ed2321a87ca006dc8bd475999c
JinHsu <1451521857@qq.com>
2020-12-02 17:53:44 +0800

    引入静态切换主题颜色

--- src/views/hello/Hello.vue
+++ src/views/hello/Hello.vue
@@ -3,11 +3,11 @@
         <h1>Hello World!</h1>
 
         <!---->
-        <el-divider/>
+        <el-divider content-position="left">起步</el-divider>
         <el-alert title="成功提示的文案" type="success"/>
 
         <!---->
-        <el-divider/>
+        <el-divider content-position="left">国际化</el-divider>
         <el-select v-model="language" placeholder="选择语言" @change="locale => $i18n.locale = locale">
             <el-option
                     v-for="item in options"
@@ -18,6 +18,19 @@
         </el-select>
         <el-divider direction="vertical"/>
         <el-date-picker/>
+
+        <!---->
+        <el-divider content-position="left">主题切换</el-divider>
+        <el-row :gutter="20">
+            <el-col :span="6">
+                <el-button type="primary">按钮</el-button>
+            </el-col>
+        </el-row>
+
+        <!---->
+        <el-divider/>
+
+
     </div>
 </template>
 
