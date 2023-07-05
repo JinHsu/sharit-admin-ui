f790b13150ed743c2aaa20157abb5f8c0c65f549
xujin129 <xujin129@crland.com.cn>
2023-07-05 22:23:23 +0800

    [feature]修改登录页面

--- src/views/entry/login/Login.vue
+++ src/views/entry/login/Login.vue
@@ -1,250 +1,105 @@
 <template>
     <div class="entry-main">
-        <a-tabs
-                :tab-bar-style="{textAlign: 'center', borderBottom: 'unset'}"
-                :active-key="activeTabKey" @change="onTabClick">
-            <a-tab-pane key="username" tab="用户名登录"/>
-            <a-tab-pane key="mobile" tab="手机号登录"/>
-        </a-tabs>
         <a-form class="form" :form="form">
             <!-- 用户名登录 -->
-            <a-form-item v-show="!isMobileLogin">
-                <a-input size="large" type="text" placeholder="用户名" autocomplete="new-password"
-                         allowClear v-decorator="['username', rules.username]">
-                    <a-icon slot="prefix" type="user" class="icon-prefix"/>
+            <a-form-item>
+                <a-input size="large" type="text" placeholder="登录名" autocomplete
+                         allowClear v-decorator="['loginName', rules.loginName]">
+                    <template #prefix>
+                        <a-icon type="user" class="icon-prefix"/>
+                    </template>
                 </a-input>
             </a-form-item>
-            <a-form-item v-show="!isMobileLogin">
-                <a-input size="large" type="password" placeholder="密码" autocomplete="new-password"
+            <a-form-item>
+                <a-input size="large" type="password" placeholder="密码" autocomplete
                          allowClear v-decorator="['password', rules.password]">
-                    <a-icon slot="prefix" type="lock" class="icon-prefix"/>
-                </a-input>
-            </a-form-item>
-
-            <!-- 手机号登录 -->
-            <a-form-item v-show="isMobileLogin">
-                <a-input size="large" type="text" placeholder="手机号" autocomplete="new-password"
-                         allowClear v-decorator="['mobile', rules.mobile]">
-                    <a-icon slot="prefix" type="mobile" class="icon-prefix"/>
-
-                </a-input>
-            </a-form-item>
-            <a-form-item v-show="isMobileLogin">
-                <a-input size="large" type="text" placeholder="验证码" autocomplete="new-password"
-                         :maxLength="6"
-                         allowClear v-decorator="['captcha', rules.captcha]">
-                    <a-icon slot="prefix" type="mail" class="icon-prefix"/>
-                    <div slot="suffix">
-                        <a-divider type="vertical"/>
-                        <span v-if="isGetCaptcha" v-text="captchaText"></span>
-                        <a @click="doGetCaptcha" class="sms-button" v-else>发送验证码</a>
-                    </div>
+                    <template #prefix>
+                        <a-icon type="lock" class="icon-prefix"/>
+                    </template>
                 </a-input>
             </a-form-item>
 
             <!--登录-->
-            <a-button size="large" type="primary" block
-                      :loading="isLogin" @click="doLogin" class="login-button">
+            <a-button size="large" type="primary" block :loading="isLogin" @click="doLogin" class="login-button">
                 登录
             </a-button>
 
-            <a-form-item class="form-other-login">
-                <span>其他登录方式</span>
-                <a @click="onOtherLogin('weibo')">
-                    <a-icon class="item-icon" type="weibo"/>
-                </a>
-                <a @click="onOtherLogin('qq')">
-                    <a-icon class="item-icon" type="qq"/>
-                </a>
-                <router-link :to="{ name: 'recover'}" class="forge-password">
-                    找回密码
-                </router-link>
-            </a-form-item>
-
         </a-form>
-<!--        <a-vaptcha ref="vaptcha" @vaptchaSuccess="onVaptchaSuccess" @vaptchaClose="onVaptchaClose"/>-->
     </div>
 </template>
 
 <script>
-    import {app} from '@/mixins'
-    import {postLogin} from '@/auth/authc'
-    import {HOME_UTL} from "@/config/auth"
-    import rules from './rules'
-    import service from './service'
+import {app} from '@/mixins'
+import {postLogin} from '@/auth/authc'
+import {HOME_UTL} from "@/config/auth"
+import rules from './rules'
 
-    export default {
-        name: "Login",
+export default {
+    name: "Login",
 
-        data() {
-            return {
-                activeTabKey: 'username',
-                form: this.$form.createForm(this),
-                rules: rules,
-                // 登录按钮状态
-                isLogin: false,
-                // captcha
-                token: '',
-                // 获取验证码按钮状态
-                isGetCaptcha: false,
-                // 验证码获取周期
-                captchaCycleTime: 60,
-            }
-        },
-
-        computed: {
-            isMobileLogin() {
-                return this.activeTabKey !== 'username'
-            },
-            captchaText() {
-                return this.captchaCycleTime + 's'
-            }
-        },
-
-        mixins: [app],
-
-        methods: {
-            onTabClick(key) {
-                this.activeTabKey = key
-                this.form.resetFields()
-                this.token = null
-                // this.$refs.vaptcha && this.$refs.vaptcha.reset()
-            },
-
-            // onVaptchaSuccess(token) {
-            //     this.token = token
-            //     this.activeTabKey === 'username' && this.doLogin()
-            //     this.activeTabKey === 'mobile' && this.doGetCaptcha()
-            // },
-
-            // onVaptchaClose() {
-            //     this.$refs.vaptcha && this.$refs.vaptcha.reset()
-            //     if (this.activeTabKey === 'username') {
-            //         this.isLogin = false
-            //     } else if (this.activeTabKey === 'mobile') {
-            //         this.isGetCaptcha = false
-            //     }
-            // },
-
-            countDown() {
-                const interval = window.setInterval(() => {
-                    if (this.captchaCycleTime-- <= 0) {
-                        this.captchaCycleTime = 60
-                        this.isGetCaptcha = false
-                        window.clearInterval(interval)
-                    }
-                }, 1000)
-            },
-
-            async onLoginSuccess() {
-                let redirect = this.$store.state.framework.multiTab.activeKey || HOME_UTL
-                await this.$router.push({path: redirect})
-
-                const {nickname} = this.userInfo || {}
-                this.$notification.success({message: `您好，${nickname}`, description: `欢迎回来`})
-            },
-
-            // 登录
-            doLogin() {
-
-                this.isLogin = true
-
-                let validateFieldsKey = this.activeTabKey === 'username' ? ['username', 'password'] : ['mobile', 'captcha']
-                // 字段校验
-                this.form.validateFields(validateFieldsKey, {force: true},
-                    (err, values) => {
-                        if (!err) {
-                            if (!this.token && this.activeTabKey === 'username') {
-                                // this.$refs.vaptcha && this.$refs.vaptcha.validate()
-                                this.isLogin = false
-                                return
-                            }
-
-                            let params = {}
-                            if (this.activeTabKey === 'username') {
-                                const {username, password} = values
-                                Object.assign(params, {username, password})
-                            } else {
-                                const {mobile, captcha} = values
-                                Object.assign(params, {mobile, captcha})
-                            }
-
-                            Object.assign(params, {vaptcha: this.token, type: this.activeTabKey})
-
-                            //
-                            postLogin(params)
-                                .then(() => this.onLoginSuccess())
-                                .finally(() => {
-                                    this.token = null
-                                    // this.$refs.vaptcha && this.$refs.vaptcha.reset()
-                                    this.isLogin = false
-                                })
-                        } else {
-                            this.isLogin = false
-                        }
-                    }
-                )
-            },
-
-            // 获取短信验证码
-            doGetCaptcha() {
-                if (this.isGetCaptcha || this.captchaCycleTime < 60) {
-                    return
-                }
-
-                this.isGetCaptcha = true
-
-                let validateFieldsKey = ['mobile']
-                // 字段校验
-                this.form.validateFields(validateFieldsKey, {force: true},
-                    (err, values) => {
-                        if (!err) {
-                            //
-                            if (!this.token && this.activeTabKey === 'mobile') {
-                                // this.$refs.vaptcha && this.$refs.vaptcha.validate()
-                                this.isGetCaptcha = false
-                                return
-                            }
-
-                            const {mobile} = values
-                            let params = {vaptcha: this.token, mobile}
-                            service.fetchCaptcha(params)
-                                .then(() => {
-                                    this.countDown()
-                                    this.$message.success("短信验证码发送成功！")
-                                })
-                                .catch(() => {
-                                    this.isGetCaptcha = false
-                                })
-                                .finally(() => {
-                                    this.token = null
-                                    // this.$refs.vaptcha && this.$refs.vaptcha.reset()
-                                })
-
-                        } else {
-                            this.isGetCaptcha = false
-                        }
-                    }
-                )
-            },
-
-            // 其他登录
-            onOtherLogin(registrationId) {
-                window.location.href = `http://localhost/oauth2/authorization/${registrationId}`
-            }
-        },
-
-        mounted() {
-            const {query: {token}} = this.$route
-            if (token) {
-                this.setAccessToken(token)
-                window.location.reload()
-            }
+    data() {
+        return {
+            form: this.$form.createForm(this),
+            rules: rules,
+            // 登录按钮状态
+            isLogin: false,
+            token: null
         }
+    },
 
+    computed: {},
+
+    mixins: [app],
+
+    methods: {
+        async onLoginSuccess() {
+            let redirect = this.$store.state.framework.multiTab.activeKey || HOME_UTL
+            await this.$router.push({path: redirect})
+
+            const {nickname} = this.userInfo || {}
+            this.$notification.success({message: `您好，${nickname}`, description: `欢迎回来`})
+        },
+
+        // 登录
+        doLogin() {
+
+            this.isLogin = true
+
+            let validateFieldsKey = this.activeTabKey === 'username' ? ['username', 'password'] : ['mobile', 'captcha']
+            // 字段校验
+            this.form.validateFields(validateFieldsKey, {force: true},
+                (err, values) => {
+                    if (!err) {
+                        let params = {}
+                        const {username, password} = values
+                        Object.assign(params, {username, password})
+                        //
+                        postLogin(params)
+                            .then(() => this.onLoginSuccess())
+                            .finally(() => {
+                                this.token = null
+                                this.isLogin = false
+                            })
+                    } else {
+                        this.isLogin = false
+                    }
+                }
+            )
+        },
+
+    },
+
+    mounted() {
+        const {query: {token}} = this.$route
+        if (token) {
+            this.setAccessToken(token)
+            window.location.reload()
+        }
     }
+
+}
 </script>
 
 <style lang="less" scoped>
-    @import "index.less";
+@import "index.less";
 </style>
--- src/views/entry/login/rules/index.js
+++ src/views/entry/login/rules/index.js
@@ -1,25 +1,13 @@
 export default {
-    username: {
+    loginName: {
         rules: [
-            {required: true, message: '请输入账号'},
+            {required: true, message: '请输入登录名'},
         ],
         validateTrigger: ['change', 'blur']
     },
     password: {
         rules: [
             {required: true, message: '请输入密码'}
-        ],
-        validateTrigger: ['change', 'blur']
-    },
-    mobile: {
-        rules: [
-            {required: true, pattern: /^1[34578]\d{9}$/, message: '请输入正确的手机号'}
-        ],
-        validateTrigger: 'change'
-    },
-    captcha: {
-        rules: [
-            {required: true, message: '请输入验证码'}
         ],
         validateTrigger: ['change', 'blur']
     }
